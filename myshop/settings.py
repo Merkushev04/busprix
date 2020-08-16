ea88e249639cb87f82c288788595945f0c249a65
Merkushev04 <polosmak.24@gmail.com>
2020-08-16 19:10:26 +0300

    16.08

--- myshop/settings.py
+++ myshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3(4y4##xe827%_ujdrd9_35r)fq)uo0bfrc-f0xw#zb9kb#n(2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
