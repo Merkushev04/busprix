44c111b6109237863a67d6931874bf3c2d7933e2
r4merkushev <r4merkushev@gmail.com>
2020-08-29 16:44:19 +0300

    update

--- myshop/settings.py
+++ myshop/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'myshop_postgres',
-#         'USER': 'Roman',
-#         'PASSWORD': 'Merkushev0412',
-#         'HOST': 'localhost', # set in docker-compose.yml
-#         'PORT': 5432 # default postgres port
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'busbprix',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
