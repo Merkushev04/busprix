301efeb8f6862203490aff5bce696fcb065f212b
r4merkushev <r4merkushev@gmail.com>
2020-10-04 13:27:27 +0300

    add foto

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3(4y4##xe827%_ujdrd9_35r)fq)uo0bfrc-f0xw#zb9kb#n(2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
